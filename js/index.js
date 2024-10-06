ee1561a5384ead4965c3ae1577e0b72f97644bbc
Manuela Gomez <manuelagomezmgg@gmail.com>
2024-10-05 20:02:40 -0600

    agregue datos

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 
+
 //let nombreIngresado = prompt("Ingresa tu nombre");
 
 //let correoIngresado = prompt("Ingresa tu correo");
@@ -19,7 +20,6 @@ let suma = 0;
 let cantidad = 0;
 let multiplicar = 0;
 let total = 0;
-let pagoTotal = 0;
 
 
  function calcularTotal (precio, cantidad){
@@ -34,11 +34,48 @@ switch (producto){
        cantidad = parseInt(prompt("Cuantos productos deseas ordenar?"))
        total = calcularTotal (producto1, cantidad);
        break;
+       case 2:
+       cantidad = parseInt(prompt("Cuantos productos deseas ordenar?"))
+       total = calcularTotal (producto2, cantidad);
+       break;
+       case 3:
+       cantidad = parseInt(prompt("Cuantos productos deseas ordenar?"))
+       total = calcularTotal (producto3, cantidad);
+       break;
+       case 4:
+       cantidad = parseInt(prompt("Cuantos productos deseas ordenar?"))
+       total = calcularTotal (producto4, cantidad);
+       break;
+       case 5:
+       cantidad = parseInt(prompt("Cuantos productos deseas ordenar?"))
+       total = calcularTotal (producto5, cantidad);
+       break;
+       case 6:
+       cantidad = parseInt(prompt("Cuantos productos deseas ordenar?"))
+       total = calcularTotal (producto6, cantidad);
+       break;
+       case 7:
+       cantidad = parseInt(prompt("Cuantos productos deseas ordenar?"))
+       total = calcularTotal (producto7, cantidad);
+       break;
+       case 8:
+       cantidad = parseInt(prompt("Cuantos productos deseas ordenar?"))
+       total = calcularTotal (producto8, cantidad);
+       break;
+       case 9:
+       cantidad = parseInt(prompt("Cuantos productos deseas ordenar?"))
+       total = calcularTotal (producto9, cantidad);
+       break;
+       default:
+         alert ("Producto invalido, por favor ingresa un numero del 1 al 9");
+         
+       }
 
-
-
-
-}
+       if (total > 0){
+         console.log("Su total es de: $ " + total)
+      } else {
+         console.log("No se selecciono ningun producto valido")
+      }
 
 
 
